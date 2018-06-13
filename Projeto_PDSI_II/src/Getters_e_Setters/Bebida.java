0cbc5a5fb9f0ed818b5ad90b09fbf730319ef9d7
Renesio <renesio1995@gmail.com>
2018-06-12 22:43:28 -0300

    Refatoraçao movendo metodos geteres e seteres para um pacote proprio

---
+++ Projeto_PDSI_II/src/Getters_e_Setters/Bebida.java
@@ -0,0 +1,71 @@
+
+package Getters_e_Setters;
+
+
+public class Bebida {
+    
+    private String Bebida;
+    private String Tipo;
+    private String Fornecedor;
+    private String Volume;
+    private float Preco;
+    private int Quantidade;
+
+    public Bebida() {
+        
+        
+    }
+
+    public String getBebida() {
+        return Bebida;
+    }
+
+    public void setBebida(String Bebida) {
+        this.Bebida = Bebida;
+    }
+
+    public String getTipo() {
+        return Tipo;
+    }
+
+    public void setTipo(String Tipo) {
+        this.Tipo = Tipo;
+    }
+    
+    public String getFornecedor() {
+        return Fornecedor;
+    }
+
+    public void setFornecedor(String Fornecedor) {
+        this.Fornecedor = Fornecedor;
+    }
+
+    public String getVolume() {
+        return Volume;
+    }
+
+    public void setVolume(String Volume) {
+        this.Volume = Volume;
+    }
+
+    public float getPreco() {
+        return Preco;
+    }
+
+    public void setPreco(float Preco) {
+        this.Preco = Preco;
+    }
+
+    public int getQuantidade() {
+        return Quantidade;
+    }
+
+    public void setQuantidade(int Quantidade) {
+        this.Quantidade = Quantidade;
+    }
+    
+    
+    
+    
+    
+}
---
+++ Projeto_PDSI_II/src/Getters_e_Setters/Fechamento_Caixa.java
@@ -0,0 +1,31 @@
+package Getters_e_Setters;
+
+public class Fechamento_Caixa {
+
+float valor,sangria,valor_total;
+
+    public float getValor() {
+        return valor;
+    }
+
+    public void setValor(float valor) {
+        this.valor = valor;
+    }
+
+    public float getSangria() {
+        return sangria;
+    }
+
+    public void setSangria(float sangria) {
+        this.sangria = sangria;
+    }
+
+    public float getValor_total() {
+        return valor_total;
+    }
+
+    public void setValor_total(float valor_total) {
+        this.valor_total = valor_total;
+    }
+    
+}
